f80b4afb862756797fabfe62dd2d5469f4d6f9a7
FaizanJatt <xmuhammadfaizanx@gmail.com>
2024-08-16 01:09:34 +0500

    added more logic for mongodb connection

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -15,7 +15,23 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
+const connectWithRetry = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB, retrying...", err);
+      setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+    });
+};
+
+connectWithRetry();
+// mongoose.connect(process.env.MONGODB_URI);
 
 // Check connection
 mongoose.connection.once("open", () => {
